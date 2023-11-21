d689feab3a9e77bb929298e00e611b9e789bc982
D4nnny <dperezf5@miumg.edu.gt>
2023-11-21 09:12:34 -0600

    Agregando Textbox y navegador SIN FUNCION

--- PrototipoEF2023_0901_20_24632/CapaVista/AsignacionCursos.Designer.cs
+++ PrototipoEF2023_0901_20_24632/CapaVista/AsignacionCursos.Designer.cs
@@ -30,30 +30,207 @@ namespace CapaVista
         private void InitializeComponent()
         {
             this.navegador1 = new NavegadorVista.Navegador();
+            this.TXT_CARRERA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TXTSEDE = new System.Windows.Forms.TextBox();
+            this.TXTJORNADA = new System.Windows.Forms.TextBox();
+            this.TXTSECCION = new System.Windows.Forms.TextBox();
+            this.TXTAULA = new System.Windows.Forms.TextBox();
+            this.TXTCURSO = new System.Windows.Forms.TextBox();
+            this.TXTCARNET = new System.Windows.Forms.TextBox();
+            this.TXTNOTA = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // navegador1
             // 
-            this.navegador1.Location = new System.Drawing.Point(122, 29);
+            this.navegador1.Location = new System.Drawing.Point(112, 12);
             this.navegador1.Name = "navegador1";
             this.navegador1.Size = new System.Drawing.Size(574, 152);
             this.navegador1.TabIndex = 0;
             this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // TXT_CARRERA
+            // 
+            this.TXT_CARRERA.Location = new System.Drawing.Point(165, 195);
+            this.TXT_CARRERA.Name = "TXT_CARRERA";
+            this.TXT_CARRERA.Size = new System.Drawing.Size(143, 20);
+            this.TXT_CARRERA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CODIGO CARRERA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CODIGO SEDE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CODIGO JORNADA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "CODIGO SECCION";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "CODIGO AULA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 355);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "CODIGO CURSO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(360, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "CARNET ALUMNO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(423, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "NOTA";
+            // 
+            // TXTSEDE
+            // 
+            this.TXTSEDE.Location = new System.Drawing.Point(165, 230);
+            this.TXTSEDE.Name = "TXTSEDE";
+            this.TXTSEDE.Size = new System.Drawing.Size(143, 20);
+            this.TXTSEDE.TabIndex = 10;
+            // 
+            // TXTJORNADA
+            // 
+            this.TXTJORNADA.Location = new System.Drawing.Point(165, 261);
+            this.TXTJORNADA.Name = "TXTJORNADA";
+            this.TXTJORNADA.Size = new System.Drawing.Size(143, 20);
+            this.TXTJORNADA.TabIndex = 11;
+            // 
+            // TXTSECCION
+            // 
+            this.TXTSECCION.Location = new System.Drawing.Point(165, 293);
+            this.TXTSECCION.Name = "TXTSECCION";
+            this.TXTSECCION.Size = new System.Drawing.Size(143, 20);
+            this.TXTSECCION.TabIndex = 12;
+            // 
+            // TXTAULA
+            // 
+            this.TXTAULA.Location = new System.Drawing.Point(165, 324);
+            this.TXTAULA.Name = "TXTAULA";
+            this.TXTAULA.Size = new System.Drawing.Size(143, 20);
+            this.TXTAULA.TabIndex = 13;
+            // 
+            // TXTCURSO
+            // 
+            this.TXTCURSO.Location = new System.Drawing.Point(165, 355);
+            this.TXTCURSO.Name = "TXTCURSO";
+            this.TXTCURSO.Size = new System.Drawing.Size(143, 20);
+            this.TXTCURSO.TabIndex = 14;
+            // 
+            // TXTCARNET
+            // 
+            this.TXTCARNET.Location = new System.Drawing.Point(466, 199);
+            this.TXTCARNET.Name = "TXTCARNET";
+            this.TXTCARNET.Size = new System.Drawing.Size(143, 20);
+            this.TXTCARNET.TabIndex = 15;
+            // 
+            // TXTNOTA
+            // 
+            this.TXTNOTA.Location = new System.Drawing.Point(466, 230);
+            this.TXTNOTA.Name = "TXTNOTA";
+            this.TXTNOTA.Size = new System.Drawing.Size(143, 20);
+            this.TXTNOTA.TabIndex = 16;
             // 
             // AsignacionCursos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TXTNOTA);
+            this.Controls.Add(this.TXTCARNET);
+            this.Controls.Add(this.TXTCURSO);
+            this.Controls.Add(this.TXTAULA);
+            this.Controls.Add(this.TXTSECCION);
+            this.Controls.Add(this.TXTJORNADA);
+            this.Controls.Add(this.TXTSEDE);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXT_CARRERA);
             this.Controls.Add(this.navegador1);
             this.Name = "AsignacionCursos";
             this.Text = "AsignacionCursos";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private NavegadorVista.Navegador navegador1;
+        private System.Windows.Forms.TextBox TXT_CARRERA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TXTSEDE;
+        private System.Windows.Forms.TextBox TXTJORNADA;
+        private System.Windows.Forms.TextBox TXTSECCION;
+        private System.Windows.Forms.TextBox TXTAULA;
+        private System.Windows.Forms.TextBox TXTCURSO;
+        private System.Windows.Forms.TextBox TXTCARNET;
+        private System.Windows.Forms.TextBox TXTNOTA;
     }
 }
